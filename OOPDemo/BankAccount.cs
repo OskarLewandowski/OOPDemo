19d242e62e57c2fb68e204b99212acc716ed707e
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-20 13:05:19 +0200

    encapsulation

---
+++ OOPDemo/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    public class BankAccount
+    {
+        //if field is private we write it be lower case
+        private float balance;
+
+        //this is the same
+        public string Name { get; set; }
+        //private string name;
+        //public void SetName(string name)
+        //{
+        //    this.name = name;
+        //}
+
+        //public string GetName()
+        //{
+        //    return this.name;
+        //}
+
+        public void MakeDeposit(float amount)
+        {
+            if (amount < 0)
+            {
+                throw new Exception("Amount must be positive number");
+            }
+
+            balance += amount;
+        }
+
+        public void MakeWithdraw(float amount)
+        {
+            if (amount < 0)
+            {
+                throw new Exception("Amount must be positive number");
+            }
+
+            balance -= amount;
+        }
+
+        public void SetBalance(float amount)
+        {
+            if (amount < 0)
+            {
+                throw new Exception("Amount must be positive number");
+            }
+
+            balance = amount;
+        }
+
+        public float GetBalance()
+        {
+            return balance;
+        }
+    }
+}
--- OOPDemo/Program.cs
+++ OOPDemo/Program.cs
@@ -20,6 +20,25 @@ namespace OOPDemo
             IPrinter printer = GetPrinter();
             printer.Print("Some content");
 
+            //encapsulation
+            BankAccount account = new BankAccount();
+            //we can't change value of this properties because is private
+            //we can when is be public but this is bad practice
+            //account.Balance = 9999999f;
+            Console.WriteLine($"Actual amount: {account.GetBalance()}");
+            account.SetBalance(99999);
+            Console.WriteLine($"Actual amount: {account.GetBalance()}");
+            account.MakeWithdraw(999);
+            Console.WriteLine($"Actual amount: {account.GetBalance()}");
+            account.MakeDeposit(25);
+            Console.WriteLine($"Actual amount: {account.GetBalance()}");
+
+            //account.SetName("Some bank account");
+            //Console.WriteLine(account.GetName());
+            account.Name = "Some bank account1";
+            Console.WriteLine(account.Name);
+
+
 
             Console.ReadLine();
         }
